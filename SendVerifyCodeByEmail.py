a81e7622927318c516029c38be4e3c535a55d958
QiaoJun <MegJoe@163.com>
2019-04-08 20:26:56 +0800

    Update SendVerifyCodeByEmail.py
    update

--- SendVerifyCodeByEmail.py
+++ SendVerifyCodeByEmail.py
@@ -1,10 +1,11 @@
+
 import smtplib
 import email.mime.multipart
 import email.mime.text
 import time, datetime, re, random
 def verifyemail(eml):
-    #msg = ''
-    s = ''
+    msg = ''
+    code = ''
     if len(eml) > 7:
         if re.match("^.+\\@(\\[?)[a-zA-Z0-9\\-\\.]+\\.([a-zA-Z]{2,3}|[0-9]{1,3})(\\]?)$", eml) != None:
             
@@ -16,62 +17,54 @@ def verifyemail(eml):
                 char=str(num)
                 verify_code.append(char)         #将每次产生的单个验证码追加到列表中。
                 i+=1
-            s="".join(verify_code)     
-            ############验证码生成完毕##############
+            code="".join(verify_code)     
+            ############验证码 => s 生成完毕##############
 
             message = email.mime.multipart.MIMEMultipart()
-            msgFrom = 'xxxx@gmail.com' #从该邮箱发送
+            msgFrom = 'example@126.com' #从该邮箱发送
             msgTo = eml #发送到该邮箱
-            smtpSever='smtp.gmail.com' # 163邮箱的smtp Sever地址
-            #smtpPort = '25' #开放的端口--163
-            smtpPort = '587'
-            sqm='xxx'  # 在登录smtp时需要login中的密码应当使用授权码而非账户密码
+            smtpSever='smtp.126.com' # 邮箱的smtp Sever地址
+            smtpPort = '25'
+            sqm='example'  # 在登录smtp时需要login中的密码应当使用 授权码 而非账户密码
             message['from'] = msgFrom
             message['to'] = msgTo
-            message['subject'] = 'TODOLIST: Please Verify Your Account '
+            message['subject'] = 'YouKnowBook: Please Verify Your Account '
+
             content = '''
-            Welcome to TODOLIST, 
-                You are using this account to login TODOLIST,
-                verify code is '''
-            content += s
+            Welcome to YouKnowBook, 
+
+            You are using this account to login YouKnowBook, we need to quickly verify your email address!   
+            the verification code is :  '''
+            content += code
             content += '''
-            You are using the email for related operations on this platform, we need to quickly verify your email address!
-            In order to keep your account secure, please do not arbitrarily disclose the verification code to others. 
-            If it is not your own operation, please ignore it.
+            In order to keep your account secure, please do not arbitrarily disclose the verification code 
+            to others. If it is not your own operation, please ignore it.
 
 
-            TODOLIST TEAM@copyright
-            toxxxst@163.com
-            Company: TODOLIST TEAM
-            Address: Beijing xxxxxations
-
+            YouKnowBook TEAM @copyright
+            example@126.com
             '''
-            #content = 'hello everyone'
             txt = email.mime.text.MIMEText(content)
             message.attach(txt)
-            smtp = smtplib
-            smtp = smtplib.SMTP()
             
-            '''
-            smtplib的connect（连接到邮件服务器）、login（登陆验证）、sendmail（发送邮件）
-            '''
-            smtp.connect(smtpSever, smtpPort)
-            smtp.ehlo()
-            smtp.starttls()
-            smtp.login(msgFrom, sqm)
-            smtp.sendmail(msgFrom, msgTo, str(message))
-            smtp.quit()
-            msg = '验证码已发送, 请及时查看邮件!'
-        else:
-            msg = '邮箱格式有误, 请检查后重试!'
-    else:
-        msg = '邮箱格式有误, 请检查后重试!'
+            try:
+                smtp = smtplib.SMTP()
+                '''
+                smtplib的connect（连接到邮件服务器）、login（登陆验证）、sendmail（发送邮件）
+                '''
+                smtp.connect(smtpSever, smtpPort)
+                smtp.login(msgFrom, sqm)
+                smtp.sendmail(msgFrom, msgTo, str(message))
+                smtp.quit()
+                msg = '验证码已发送, 请及时查看邮件!'
+            except smtplib.SMTPException as e:
+                msg = e
 
     output = {
-        'verify_code' : s,
+        'verify_code' : code,
         'msg' : msg,
     }
     return output
 
-#测试
-print (verifyemail('10xxxx9@qq.com'))
+print (verifyemail('example@163.com'))
+
